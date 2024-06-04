338098f18abaf0c7bcc4bab6b775917b3173c1a3
charan <charanmalli222@gmail.com>
2024-06-04 12:21:37 +0530

    release 22.01 with plugins.

---
+++ plugins/atparOfbizApp/src/main/java/com.company.atparOfbizApp/services/AtparOfbizAppServices.java
@@ -0,0 +1,45 @@
+package com.companyname.atparOfbizApp.services;
+import java.util.Map;
+
+import org.apache.ofbiz.base.util.Debug;
+import org.apache.ofbiz.entity.Delegator;
+import org.apache.ofbiz.entity.GenericEntityException;
+import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.service.DispatchContext;
+import org.apache.ofbiz.service.ServiceUtil;
+
+public class AtparOfbizAppServices {
+
+    public static final String module = AtparOfbizAppServices.class.getName();
+
+    public static Map<String, Object> createAtparTechie(DispatchContext dctx, Map<String, ? extends Object> context) {
+        Map<String, Object> result = ServiceUtil.returnSuccess();
+        Delegator delegator = dctx.getDelegator();
+        try {
+            GenericValue atparTechie = delegator.makeValue("atparTechie");
+            // Auto generating next sequence of ofbizDemoId primary key
+            atparTechie.setNextSeqId();
+            // Setting up all non primary key field values from context map
+            atparTechie.setNonPKFields(context);
+            // Creating record in database for OfbizDemo entity for prepared value
+            atparTechie = delegator.create(atparTechie);
+            result.put("atparTechieId", atparTechie.getString("atparTechieId"));
+            Debug.log("==========This is my first Java Service implementation in Apache OFBiz. AtparTechie record created successfully with atparTechieId: "+atparTechie.getString("atparTechieId"));
+
+
+            try {
+                Debug.logInfo("=======Creating OfbizDemo record in event using service createOfbizDemoByGroovyService=========", module);
+                dispatcher.runSync("createAtparTechie", UtilMisc.toMap("ofbizDemoTypeId", ofbizDemoTypeId,
+                        "firstName", firstName1, "lastName", lastName, "comments", comments, "userLogin", userLogin));
+            } catch (GenericServiceException e) {
+                String errMsg = "Unable to create new records in OfbizDemo entity: " + e.toString();
+                request.setAttribute("_ERROR_MESSAGE_", errMsg);
+                return "error";
+            }
+        } catch (GenericEntityException e) {
+            Debug.logError(e, module);
+            return ServiceUtil.returnError("Error in creating record in OfbizDemo entity ........" +module);
+        }
+        return result;
+    }
+}
