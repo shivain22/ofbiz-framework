f979f0eb406e85b8fa4aa9934edcc22c725d6c9e
Nicolas Malin <nicolas.malin@nereide.fr>
2024-11-19 10:36:28 +0100

    Improved: Update end bracket detection en groovy scriplet (OFBIZ-13178)
    When you use double brackets on groovy scriptlet, the end bracket isn't correctly manage.
    
    The follow simple code failed :
    
    ${groovy: if (true) {return 0}}

--- framework/base/src/test/java/org/apache/ofbiz/base/util/string/FlexibleStringExpanderTests.java
+++ framework/base/src/test/java/org/apache/ofbiz/base/util/string/FlexibleStringExpanderTests.java
@@ -316,8 +316,12 @@ public class FlexibleStringExpanderTests {
         fseTest("UEL integration: missing", "${noList[0]}", testMap, null, null, "", null, false);
         fseTest("Escaped expression", "This is an \\${escaped} expression", testMap, "This is an ${escaped} expression", false);
         fseTest("Escaped(groovy) expression", "This is an \\${groovy:escaped} expression", testMap, "This is an ${groovy:escaped} expression", false);
+        fseTest("Bracket en groovy", "This is a groovy ${groovy: if (true) {return 'bracket'}} expression", testMap,
+                "This is a groovy bracket expression", false);
+        fseTest("Bracket en groovy again", "This is a groovy ${groovy: if (true) {if (true) {return 'with 2 brackets'}}} expression", testMap,
+                "This is a groovy with 2 brackets expression", false);
 
-        // TODO: Find a better way to setup or handle the big decimal value. If new ones are not instanciated in the test
+        // TODO: Find a better way to setup or handle the big decimal value. If new ones are not instantiated in the test
         // it fails because of the comparison between object pointers..
         fseTest("nested UEL integration(return BigDecimal)", "${a${'moun'}t}", testMap, null, LOCALE_TO_TEST,
                 "1,234,567.89", new BigDecimal("1234567.89"), false);
